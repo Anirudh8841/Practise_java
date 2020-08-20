98232d5f67f7bff76e32ddf75e297400cb5e6d44
Anirudh8841 <anirudh8841@gmail.com>
2020-08-20 18:41:12 +0530

    added runscript

---
+++ runscript.bat
@@ -0,0 +1,11 @@
+javac SingleInheritance.java
+java Main
+javac MultiLevelInheritance.java
+java Main
+javac hierarchialInheritance.java
+java Main
+javac MultipleInheritance.java
+java Main
+javac Encapsulation.java
+java Main
+Pause
