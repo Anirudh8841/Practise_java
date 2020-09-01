60c0135b8101e3c1ca0b8915edbe30a6ba589f5c
Anirudh8841 <anirudh8841@gmail.com>
2020-09-01 12:43:55 +0530

    updated runscript

--- runscript.bat
+++ runscript.bat
@@ -10,10 +10,22 @@ javac Encapsulation.java
 java Main
 javac Abstraction.java
 java Main
-javac RunTimePolymorphism.java
+javac RunTImePolymorphism.java
 java Main
 javac CompileTimePolymorphism.java
 java Main
 javac thisRef.java
+java Main
+javac misPart1.java
+java Main
+javac misPart2.java
+java Main
+javac misPart3.java
+java Main
+javac misPart4.java
+java base
+javac strictfp.java
+java Main
+javac super.java
 java Main
 Pause
